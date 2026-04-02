8b08223225110fa7d6254efaa5e7d3f4dfd7f915
IvanDing <ivan.ding95@qq.com>
2026-04-03 01:47:55 +0800

    升级到kbenex

--- scripts/guiconsole.bat
+++ scripts/guiconsole.bat
@@ -1,16 +1,22 @@
 @echo off
-set curpath=%~dp0
+cd ..
+set PROJECT_PATH=%cd%
 
 cd ..
-set KBE_ROOT=%cd%
-set KBE_RES_PATH=%KBE_ROOT%/kbe/res/;%curpath%/;%curpath%/scripts/;%curpath%/res/
+
+if not defined KBE_ROOT (
+    set KBE_ROOT=%cd%
+)
+
+set KBE_RES_PATH=%KBE_ROOT%/kbe/res/;%PROJECT_PATH%/;%PROJECT_PATH%/res/
 set KBE_BIN_PATH=%KBE_ROOT%/kbe/bin/server/
 
-set uid=%random%%%32760+1
 
+cd %~dp0
+
+echo PROJECT_PATH = %PROJECT_PATH%
 echo KBE_ROOT = %KBE_ROOT%
 echo KBE_RES_PATH = %KBE_RES_PATH%
 echo KBE_BIN_PATH = %KBE_BIN_PATH%
-
 cd %KBE_ROOT%/kbe/tools/server/guiconsole/
 start guiconsole.exe
